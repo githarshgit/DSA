123db9b5a95579bb2ffb8c46c45e49ea404a2217
Harsh <projectsbyharsh@gmail.com>
2026-01-18 21:55:59 +0530

    optimal_aproach for palindrome

--- C++ basics/recursion/Check_if_the_given_String_is_Palindrome_or_not.cpp
+++ C++ basics/recursion/Check_if_the_given_String_is_Palindrome_or_not.cpp
@@ -40,3 +40,21 @@ int main() {
 
     return 0;
 }
+
+
+
+// Optimal approch using recursion
+
+bool palindrome(int i, string& s) {
+    // Base Condition: If i exceeds half of the string, all the elements have been compared
+    // and the string is a palindrome, return true.
+    if (i >= s.length() / 2) return true;
+
+    // If the start and end characters are not equal, it's not a palindrome.
+    if (s[i] != s[s.length() - i - 1]) return false;
+
+    // If both characters are the same, increment i and check start+1 and end-1.
+    return palindrome(i + 1, s);
+}
+
+
