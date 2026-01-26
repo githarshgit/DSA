02dbd8258ce90490ef9aa8afa536ccdc01fcbc2d
Harsh <projectsbyharsh@gmail.com>
2026-01-27 00:18:28 +0530

    rotate_array_dsa

---
+++ array/rotate_array.cpp
@@ -0,0 +1,33 @@
+//bruteforce 
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        for(int i = 0; i < k; i++) {
+            int last = nums[n - 1];
+            for(int j = n - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = last;
+        }
+    }
+};
+
+
+// optimized
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;   // VERY IMPORTANT
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
